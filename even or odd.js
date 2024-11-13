02af6192536756b43338d67d0ac118b00907048a
P4lk4 <168112469+P4lk4@users.noreply.github.com>
2024-11-12 22:11:24 -0600

    Add files via upload
    It's just a test

---
+++ even or odd.js
@@ -0,0 +1,8 @@
+var num1 = 2;
+var num2 = 5;
+var test1 = num1 % 2;
+var test2 = num2 % 2;
+var res1 = test1 == 0;
+var res2 = test2 == 0;
+console.log("The number", num1, "it's even", res1);
+console.log("The number", num2, "it's even", res2);
